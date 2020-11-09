4759ad90cc53f54d7622909161f73a490ce22129
Marco Giulietti <ga6t00n@gmail.com>
2020-11-09 15:50:40 +0100

    Esercizio fatto [+ Bonus]

--- js/main.js
+++ js/main.js
@@ -16,6 +16,9 @@ const app = new Vue ({
         ],
         intervalID: 0
     },
+    created() { //Genero un loop al caricamento della pagina
+        this.startLoop();
+    },
     methods: {
         nextPhoto() {
             this.indexPhoto ++;
@@ -30,5 +33,18 @@ const app = new Vue ({
                this.indexPhoto = (this.photos.length - 1);
            }
        },
+
+       setPhoto(index) {
+           this.indexPhoto = index;
+       },
+
+       startLoop() {
+           this.intervalId = setInterval(()=> {
+               this.nextPhoto();
+           }, 3000);
+       },
+       stopLoop() {
+           clearInterval(this.intervalId);
+       }
     }
 });
