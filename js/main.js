e9fdedddee35d5ac3b5ea14c2371293e438d5ddb
Marco Giulietti <ga6t00n@gmail.com>
2020-11-09 15:38:15 +0100

    Base JS work

--- js/main.js
+++ js/main.js
@@ -3,3 +3,32 @@
  * Vue Slider
  * 
  */
+
+const app = new Vue ({
+    el: '#app',
+    data: {
+        indexPhoto: 0,
+        photos: [
+            './images/image1.jpg',
+            './images/image2.jpg',
+            './images/image3.jpg',
+            './images/image4.jpg',
+        ],
+        intervalID: 0
+    },
+    methods: {
+        nextPhoto() {
+            this.indexPhoto ++;
+            if (this.indexPhoto > (this.photos.length - 1)) {
+                this.indexPhoto = 0;
+            }
+        },
+
+        prevPhoto() {
+           this.indexPhoto --;
+           if (this.indexPhoto < 0) {
+               this.indexPhoto = (this.photos.length - 1);
+           }
+       },
+    }
+});
